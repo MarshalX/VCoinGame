2933e1c86846734e5abd1c898cb639017b134a8d
DarkKeks <darkkeks@rambler.ru>
2019-04-16 00:08:51 +0300

    -.-

--- main.py
+++ main.py
@@ -507,7 +507,7 @@ if __name__ == '__main__':
         transactions = coin_api.get_transactions()
         transactions.extend(coin_api.get_transactions(False))
         transactions = [transaction for transaction in transactions \
-                        if transaction.from_id != merchant_id and transaction.payload == CoinAPI.PAYLOAD]
+                        if transaction.from_id != merchant_id and transaction.payload == int(CoinAPI.PAYLOAD)]
 
         for transaction in transactions:
             if transaction.id not in all_transactions:
